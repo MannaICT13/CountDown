5e06f2a6b260e1e3dd7fd49530d863f892bfe1cd
Manna <khmannaict13@gmail.com>
2019-07-03 22:15:57 +0600

    commit all

--- CountDown/ViewController.swift
+++ CountDown/ViewController.swift
@@ -1,25 +1,62 @@
-//
-//  ViewController.swift
-//  CountDown
-//
-//  Created by USER on 5/19/18.
-//  Copyright © 2018 mCubes. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startButtonOutlet: UIButton!
+    
+    @IBOutlet weak var resumeButtonOutlet: UIButton!
+    
+    @IBOutlet weak var pauseButtonOutlet: UIButton!
+    
+    @IBOutlet weak var resetButtonOutlet: UIButton!
+    
+    @IBOutlet weak var imageViewOutlet: UIImageView!
+    
+    @IBOutlet weak var coundownOutlet: UILabel!
+    
+    @IBOutlet weak var timesupOutlet: UILabel!
+    
+    @IBOutlet weak var secondsLabelOutlet: UILabel!
+    
+    
+    var seconds = 10
+    var timer : Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       imageViewOutlet.isHidden = true
+        timesupOutlet.isHidden = true
+        resetButtonOutlet.isHidden = true
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    @IBAction func startAction(_ sender: UIButton) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: <#T##Selector#>, userInfo: nil, repeats: true)
     }
-
+    
+    
+    @IBAction func pauseAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func resumeAction(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func resetAction(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
 
 }
 
